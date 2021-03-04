69331fa6a95d4832445693314c8de25501de3b89
shilpa Malempati <shilpamalempati443@gmail.com>
2021-03-04 18:23:08 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root'),
+  
+// );
+
+// class: 1
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+
+// class 2: SET PROPETIES IN REACT COMPONENT
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// ReactDOM.render(
+//   <App cat={5} txt="hello shilpa"/>,
+//    document.getElementById('app')
+//   );
+
+
+
+// class 3-12: manage react Component state with setstate
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render(
+  <App/>,
+   document.getElementById('app')
+  );
+
+
